7e370633bc64f3c84a5a51991e7b63686d6df025
Azlin Hairani Subki <36842623+AzlinHS@users.noreply.github.com>
2018-06-21 16:06:37 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,41 @@
+## setting up the default
+knitr::opts_chunk$set(echo = TRUE, results = "asis")
+
+## set the value of the vector
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+## get the value of the vector
+  get <- function() x
+  
+## set the value of the mean
+  setmean <- function(mean) m <<- mean
+  
+## get the value of the mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+
 ## A pair of functions that cache the inverse of a matrix.
 ## This function creates a special "matrix" object that can cache its inverse.
 
